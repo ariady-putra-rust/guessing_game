651f5ad73d7105c5544e18f1a23be1a32a2d3acd
Ariady Putra <ariady.putra@gmail.com>
2023-05-25 20:05:25 +0700

    Loop label

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,25 @@ fn main() {
     // let c = [true; 3]; // [true, true, true]
     // println!("{}", c[1]);
 
+    /* Loop label */
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {count}");
+    //     let mut remaining = 10;
+    //     loop {
+    //         println!("remaining = {remaining}");
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -= 1;
+    //     }
+    //     count += 1;
+    // }
+    // println!("End count = {count}");
+
     let secret_numbers: [u8; 5] = [
         rand::thread_rng().gen_range(1..=100),
         rand::thread_rng().gen_range(1..=100),
