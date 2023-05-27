52f129496785bad052d6b7d33fb2367df75de2d8
Ariady Putra <ariady.putra@gmail.com>
2023-05-27 21:48:34 +0700

    Dangling References

---
+++ src/dangling_references/mod.rs
@@ -0,0 +1,13 @@
+/* Dangling References */
+pub fn dangling_references() {
+    // let reference_to_nothing = dangle();
+}
+
+// fn dangle() -> &String {
+//     // `dangle` returns a reference to a String
+
+//     let s = String::from("hello"); // `s` is a new String
+
+//     &s // we return a reference to the String `s`
+// } // Here `s` goes out of scope and is dropped. Its memory goes away.
+//   // Danger!
