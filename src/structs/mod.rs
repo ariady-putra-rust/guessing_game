de2a141ce9f6eaa60561a05cab7668361a222c74
Ariady Putra <ariady.putra@gmail.com>
2023-05-31 23:28:20 +0700

    Implementing Struct

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,11 +1,30 @@
 /* Unit Struct */
 pub struct Unit;
 /*
-Unit-like structs can be useful when you need to implement a trait on some type but don’t have any data that you want to store in the type itself
+Unit-like structs can be useful when you need to implement a trait on some type but don't have any data that you want to store in the type itself
 */
 
 /* Tuple Struct */
 pub struct XYZ(u8, u8, u8);
+
+impl XYZ {
+    pub fn new(x: u8, y: u8, z: u8) -> Self {
+        Self(x, y, z)
+    }
+
+    pub fn x(&self) -> u8 {
+        self.0
+    }
+
+    pub fn y(&self) -> u8 {
+        self.1
+    }
+
+    pub fn z(&self) -> u8 {
+        self.2
+    }
+}
+
 pub fn xyz() {
     let xyz = XYZ(5, 4, 3);
     println!("{} {} {}", xyz.0, xyz.1, xyz.2);
