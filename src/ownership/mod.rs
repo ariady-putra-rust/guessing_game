bfb9e29d444585adaac440ac88bb91214bff49c3
Ariady Putra <ariady.putra@gmail.com>
2023-05-31 22:20:19 +0700

    Sync

--- src/ownership/mod.rs
+++ src/ownership/mod.rs
@@ -2,7 +2,7 @@
 // 1. Each value in Rust has an owner.
 // 2. There can only be one owner at a time.
 // 3. When the owner goes out of scope, the value will be dropped.
-pub fn main() {
+pub fn ownership() {
     ownership_and_functions();
     transferring_ownership_of_return_values();
 }
