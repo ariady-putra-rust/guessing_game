3a3e5e4f1cd185da52bef961353e051aea7be8a1
Ariady Putra <ariady.putra@gmail.com>
2023-06-02 03:20:50 +0700

    `ToVecString` unit test

--- lib/file_access/src/traits/impls/to_vec_string.rs
+++ lib/file_access/src/traits/impls/to_vec_string.rs
@@ -5,3 +5,34 @@ impl<Line: AsRef<str>> ToVecString for Vec<Line> {
         self.iter().map(|line| line.as_ref().to_string()).collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::any::{Any, TypeId};
+
+    fn is_vec_string(a: &dyn Any) -> bool {
+        a.type_id() == TypeId::of::<Vec<String>>()
+    }
+
+    #[test]
+    fn vec_str_to_vec_string() -> Result<()> {
+        Ok({
+            // Arrange
+            let vec_str = vec!["hello", "world"];
+
+            // Action
+            let vec_string = vec_str.to_vec_string();
+
+            // Assert
+            assert!(
+                is_vec_string(&vec_string),
+                "vec_string should be a Vec<String>"
+            );
+            assert!(
+                !is_vec_string(&vec_str),
+                "vec_str should NOT be a Vec<String>"
+            );
+        })
+    }
+}
