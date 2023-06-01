ba1ae507dd6fddda62a08e4cdc9409c614833e9e
Ariady Putra <ariady.putra@gmail.com>
2023-06-01 21:21:10 +0700

    `Line` as `AsRef<str>`

--- lib/file_access/src/traits/impls/to_vec_string.rs
+++ lib/file_access/src/traits/impls/to_vec_string.rs
@@ -1,6 +1,6 @@
 use crate::{traits::to_vec_string::*, *};
 
-impl<T: AsRef<str>> ToVecString<T> for Vec<T> {
+impl<Line: AsRef<str>> ToVecString<Line> for Vec<Line> {
     fn to_vec_string(&self) -> Vec<Text> {
         self.iter().map(|line| line.as_ref().to_string()).collect()
     }
